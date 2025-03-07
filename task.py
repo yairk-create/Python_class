a9b14bdd7872567cce3499cc87393349dfac2d01
kochaviyair <kochavi.yair@gmail.com>
2025-03-07 12:15:21 +0200

    task.py comments

--- task.py
+++ task.py
@@ -1,3 +1,6 @@
+
+#pratctice 1
+
 # name= input( "Please enter your name: " )
 
 # age= input ("Enter your age: ")
@@ -9,34 +12,39 @@
 # print (age.index('"'))
 
 
-a= 1
-b=2
-c=3
-d=4
 
 
-if a > b:
-    print("Hello World"  )
+
+#pratctice 2
+
+# a= 1
+# b=2
+# c=3
+# d=4
+
+
+# if a > b:
+#     print("Hello World"  )
     
-if a != b:
-    print( "Hello World" )
+# if a != b:
+#     print( "Hello World" )
 
-if  a == b:
-    print ("YES")    
-else:
-    print ("NO")
+# if  a == b:
+#     print ("YES")    
+# else:
+#     print ("NO")
 
-if a == b:
-    print ("1")
-elif a > b:
-    print ("2")
-else:
-    print ("3")
+# if a == b:
+#     print ("1")
+# elif a > b:
+#     print ("2")
+# else:
+#     print ("3")
     
-if a == b and c == d:
-    print ("Hello")
-if a == b or c == d:
-    print ("Hello")
+# if a == b and c == d:
+#     print ("Hello")
+# if a == b or c == d:
+#     print ("Hello")
 
-if 5 > 2:
-    print ("Five is greater than two!")
+# if 5 > 2:
+#     print ("Five is greater than two!")

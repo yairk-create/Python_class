cac1c343a655bc74bbcd5291f5ed94056f9d03a7
kochaviyair <kochavi.yair@gmail.com>
2025-03-07 12:08:20 +0200

    python

--- task.py
+++ task.py
@@ -1,10 +1,42 @@
-name= input( "Please enter your name: " )
+# name= input( "Please enter your name: " )
 
-age= input ("Enter your age: ")
-age='"'+age+'"'
-print (name, "\\" )
+# age= input ("Enter your age: ")
+# age='"'+age+'"'
+# print (name, "\\" )
 
-print (age    )
+# print (age    )
 
-print (age.index('"'))
+# print (age.index('"'))
 
+
+a= 1
+b=2
+c=3
+d=4
+
+
+if a > b:
+    print("Hello World"  )
+    
+if a != b:
+    print( "Hello World" )
+
+if  a == b:
+    print ("YES")    
+else:
+    print ("NO")
+
+if a == b:
+    print ("1")
+elif a > b:
+    print ("2")
+else:
+    print ("3")
+    
+if a == b and c == d:
+    print ("Hello")
+if a == b or c == d:
+    print ("Hello")
+
+if 5 > 2:
+    print ("Five is greater than two!")
